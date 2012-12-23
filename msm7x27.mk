5ddc8abbb6777d56e6b808d01ff41959a315fdf0
GenetICS <gntcs@msn.com>
2012-12-23 14:58:46 +0200

    Remove ambient light sensor permisson

--- msm7x27.mk
+++ msm7x27.mk
@@ -55,6 +55,5 @@ PRODUCT_COPY_FILES += \
          frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
          frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
          frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-         frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
          frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
          frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
