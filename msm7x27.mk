93f14681caa191a2f70220a61cc80c5c3a5cf6be
GenetICS <gntcs@msn.com>
2012-12-23 13:01:44 +0200

    Add msm7x27 common packages and properties

--- msm7x27.mk
+++ msm7x27.mk
@@ -1,2 +1,47 @@
 $(call inherit-product, vendor/qcom/msm7x27/qcom-vendor.mk)
 
+# Camcorder
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.camcorder.disablemeta=1
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true \
+    ro.opengles.version=131072 \
+    debug.enabletr=false \
+    debug.hwui.render_dirty_regions=false \
+    debug.qctwa.statusbar=1 \
+    debug.qctwa.preservebuf=1 \
+    hwui.print_config=choice \
+    persist.sys.strictmode.visual=false
+
+# Stagefright
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-meta=false \
+    media.stagefright.enable-scan=false \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaudioutils
+
+# Display
+PRODUCT_PACKAGES += \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer \
+    gralloc.msm7x27 \
+    copybit.msm7x27
+
+# Media
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libmm-omxcore \
+    libOmxCore
+
