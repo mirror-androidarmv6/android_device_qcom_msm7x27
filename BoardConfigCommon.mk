144593bd0703521d4167ee70d5209f1c550fcdee
AndroidMeda <hephappy@gmail.com>
2013-11-28 09:59:09 +0100

    Use legacy variants for hardware
    Change-Id: I9013c0d975530994bc6d9049493e26e45dc7484d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,46 +15,47 @@
 #
 
 # Target
-TARGET_ARCH := arm
-TARGET_ARCH_LOWMEM := true
-TARGET_ARCH_VARIANT := armv6-vfp
-TARGET_BOARD_PLATFORM := msm7x27
-TARGET_CPU_ABI := armeabi
-TARGET_CPU_ABI2 := armeabi-v6l
-TARGET_CPU_VARIANT := arm11
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
+TARGET_ARCH            := arm
+TARGET_ARCH_LOWMEM     := true
+TARGET_ARCH_VARIANT    := armv6-vfp
+TARGET_BOARD_PLATFORM  := msm7x27
+TARGET_CPU_ABI         := armeabi
+TARGET_CPU_ABI2        := armeabi-v6l
+TARGET_CPU_VARIANT     := arm11
+TARGET_NO_BOOTLOADER   := true
+TARGET_NO_RADIOIMAGE   := true
+COMMON_GLOBAL_CFLAGS   += -DTARGET_MSM7x27
 
 # For assembly error
-# COMMON_GLOBAL_CFLAGS += --save-temps
+#COMMON_GLOBAL_CFLAGS   += --save-temps
 
 # Graphics
-USE_OPENGL_RENDERER := true
-TARGET_NO_HW_OVERLAY := true
-TARGET_QCOM_DISPLAY_VARIANT := legacy
-BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
-BOARD_EGL_CFG := device/qcom/msm7x27/egl.cfg
-BOARD_EGL_NEEDS_LEGACY_FB := true
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QCOM_LIBS := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-#COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
-COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
-COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
+USE_OPENGL_RENDERER                     := true
+TARGET_NO_HW_OVERLAY                    := true
+TARGET_QCOM_DISPLAY_VARIANT             := legacy
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET      := true
+BOARD_EGL_CFG                           := device/qcom/msm7x27/egl.cfg
+BOARD_EGL_NEEDS_LEGACY_FB               := true
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE                := true
+BOARD_USES_QCOM_LIBS                    := true
+COMMON_GLOBAL_CFLAGS                    += -DQCOM_HARDWARE
+#COMMON_GLOBAL_CFLAGS                    += -DQCOM_ICS_COMPAT
+
+# Video
+TARGET_QCOM_LEGACY_OMX                  := true
+TARGET_QCOM_MEDIA_VARIANT               := legacy
+TARGET_ENABLE_QC_AV_ENHANCEMENTS        := true
+COMMON_GLOBAL_CFLAGS                    += -DQCOM_NO_SECURE_PLAYBACK
 
 # Camera
-TARGET_DISABLE_ARM_PIE := true
-
-# Bluetooth
-ifneq (true,$(BOARD_HAVE_BLUETOOTH_BLUEZ))
-BOARD_HAVE_BLUETOOTH := true
-endif
+TARGET_DISABLE_ARM_PIE          := true
+BOARD_NEEDS_MEMORYHEAPPMEM      := true
 
 # Javascript, Browser and Webkit
-WITH_JIT := true
-ENABLE_JSC_JIT := true
-JS_ENGINE := v8
-HTTP := chrome
+WITH_JIT                := true
+ENABLE_JSC_JIT          := true
+JS_ENGINE               := v8
+HTTP                    := chrome
 TARGET_FORCE_CPU_UPLOAD := true
